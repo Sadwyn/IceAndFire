4593579d9f36b1e23b78005ada506285a6b8d3fe
Sadwyn <sadwyn1@gmail.com>
2017-03-31 18:10:15 +0300

    text view wrapped in cardview

--- app/src/main/java/com/sadwyn/iceandfire/Constants.java
+++ app/src/main/java/com/sadwyn/iceandfire/Constants.java
@@ -10,9 +10,11 @@ public interface Constants {
     String LANG_PREF = "LANG_PREF";
     String DATA_SOURCE_PREF = "DATA_SOURCE_PREF";
     String CHARACTER_KEY = "CHARACTER_KEY";
+    //fragment Constants
     String CHARACTERS_FRAGMENT_TAG = "CHARACTERS_FRAGMENT_TAG";
     String DETAIL_FRAGMENT_TAG = "DETAIL_FRAGMENT_TAG";
     String SETTINGS_FRAGMENT_TAG = "SETTINGS_FRAGMENT_TAG";
+    //widget Constants
     String SEND_TO_SERVICE_KEY = "SEND_TO_SERVICE_KEY";
     String WIDGET_INFO_GOTH = "WIDGET_INFO_GOTH";
     String PREV_HERO_SWITCH = "PREV_HERO_SWITCH";
@@ -29,7 +31,6 @@ public interface Constants {
     String INSTANT_ID_REQUEST = "INSTANT_ID_REQUEST";
     String INSTANT_HERO_ID = "INSTANT_HERO_ID";
     String INSTANT_HERO_NAME = "INSTANT_HERO_NAME";
-
     String HERO_START_ID = "HERO_START_ID";
     String CURRENT_LIST_ID = "CURRENT_LIST_ID";
     String COM_SADWYN_UPDATE_WIDGET = "com.sadwyn.update.widget";
